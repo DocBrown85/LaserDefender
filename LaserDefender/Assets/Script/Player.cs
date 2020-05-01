f4c7b71bc31680874681054d8d28828edcc2ae98
docbrown85 <me@example.com>
2020-05-01 12:16:43 +0200

    Add player movement

--- LaserDefender/Assets/Script/Player.cs
+++ LaserDefender/Assets/Script/Player.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Configuration Parameters
+    [SerializeField] float moveSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        // Multiply times Time.deltaTime to make the movement frame rate independent
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPosition = transform.position.x + deltaX;
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPosition = transform.position.y + deltaY;
+
+        transform.position = new Vector2(newXPosition, newYPosition);
     }
 }
