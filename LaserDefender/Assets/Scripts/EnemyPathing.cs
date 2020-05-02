fc5961af01587d746345dc7e49ec2aa1efaaeead
docbrown85 <me@example.com>
2020-05-02 13:06:33 +0200

    Spawn multiple enemy waves

--- LaserDefender/Assets/Scripts/EnemyPathing.cs
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
     // Configuration Parameters
-    [SerializeField] WaveConfig waveConfig;
-    [SerializeField] float moveSpeed = 2.0f;
 
     // Object State
     int waypointIndex = 0;
     List<Transform> waypoints;
+    WaveConfig waveConfig;
 
     // Start is called before the first frame update
     void Start()
@@ -26,12 +25,17 @@ public class EnemyPathing : MonoBehaviour
         Move();
     }
 
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
     private void Move()
     {
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
 
             if (transform.position == targetPosition)
