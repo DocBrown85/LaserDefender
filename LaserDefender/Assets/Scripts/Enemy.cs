be9146ccc8b5b5b2223324b90353051ac825c0a3
docbrown85 <me@example.com>
2020-05-03 13:26:54 +0200

    Destroy enemy when health reaches 0

--- LaserDefender/Assets/Scripts/Enemy.cs
+++ LaserDefender/Assets/Scripts/Enemy.cs
@@ -19,8 +19,16 @@ public class Enemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
 
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
         health -= damageDealer.GetDamage();
-        
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
