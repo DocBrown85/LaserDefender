e75d786911f43a834f0d7fbc3286e4a9de5fd810
docbrown85 <me@example.com>
2020-05-03 13:53:30 +0200

    Allow enemy to shoot

--- LaserDefender/Assets/Scripts/Enemy.cs
+++ LaserDefender/Assets/Scripts/Enemy.cs
@@ -1,19 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 10f;
 
     private void Start()
     {
-        
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     private void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        // projectileSpeed is negative because the enemy has to shoot downwards
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1 * projectileSpeed);
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
